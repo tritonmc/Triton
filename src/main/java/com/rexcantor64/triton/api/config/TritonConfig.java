9793c121fb68bcddf6b2e717422c6fce97166b8e
Diogo Correia <me@diogotc.com>
2020-03-09 09:48:31 +0000

    Add terminal translation (#75)

--- src/main/java/com/rexcantor64/triton/api/config/TritonConfig.java
+++ src/main/java/com/rexcantor64/triton/api/config/TritonConfig.java
@@ -147,6 +147,12 @@ public interface TritonConfig {
     boolean isBossbars();
 
     /**
+     * @return The value of "language-creation.terminal" in the config.
+     * @since 2.6.0
+     */
+    boolean isTerminal();
+
+    /**
      * @return The {@link com.rexcantor64.triton.api.config.FeatureSyntax FeatureSyntax} of "language-creation.chat"
      * in the config.
      * @since 1.0.0
