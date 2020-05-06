07858ad379dde7dbc32f006fe64b8fb075d59e99
Diogo Correia <me@diogotc.com>
2020-05-06 23:58:02 +0100

    Add 1.13 through 1.15 modules boilerplate
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ v1_13/src/main/java/com/rexcantor64/triton/v1_13/MaterialWrapper.java
@@ -0,0 +1,11 @@
+package com.rexcantor64.triton.v1_13;
+
+import org.bukkit.Material;
+
+public class MaterialWrapper {
+
+    public static Material getBannerMaterial() {
+        return Material.WHITE_BANNER;
+    }
+
+}
