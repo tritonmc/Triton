7c6b92108962686f5d9803c04df80291bc9756a3
Diogo Correia <me@diogotc.com>
2020-05-06 23:44:53 +0100

    Generated modules boilerplate
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ spigot-legacy/src/main/java/com/rexcantor64/triton/legacy/MaterialWrapper.java
@@ -0,0 +1,11 @@
+package com.rexcantor64.triton.legacy;
+
+import org.bukkit.Material;
+
+public class MaterialWrapper {
+
+    public static Material getBannerMaterial() {
+        return Material.BANNER;
+    }
+
+}
