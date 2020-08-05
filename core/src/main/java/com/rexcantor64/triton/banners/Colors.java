f34202b7269cda8bef4c72f9faf598bdf60e00be
Diogo Correia <me@diogotc.com>
2020-08-05 14:29:11 +0100

    Fix DyeColor SILVER change in Spigot 1.13 (fixes #112)
    Fixes https://github.com/tritonmc/Triton/issues/112
    
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- core/src/main/java/com/rexcantor64/triton/banners/Colors.java
+++ core/src/main/java/com/rexcantor64/triton/banners/Colors.java
@@ -1,8 +1,11 @@
 package com.rexcantor64.triton.banners;
 
+import com.rexcantor64.triton.Triton;
+
 public enum Colors {
     BLACK("BLACK", 'a'), RED("RED", 'b'), GREEN("GREEN", 'c'), BROWN("BROWN", 'd'), BLUE(
-            "BLUE", 'e'), PURPLE("PURPLE", 'f'), CYAN("CYAN", 'g'), GRAY("SILVER",
+            "BLUE", 'e'), PURPLE("PURPLE", 'f'), CYAN("CYAN", 'g'), GRAY(Triton.get()
+            .getMcVersion() <= 12 ? "SILVER" : "LIGHT_GRAY",
             'h'), DARK_GRAY("GRAY", 'i'), PINK("PINK", 'j'), LIME("LIME", 'k'), YELLOW(
             "YELLOW", 'l'), LIGHT_BLUE("LIGHT_BLUE", 'm'), MAGENTA("MAGENTA",
             'n'), ORANGE("ORANGE", 'o'), WHITE("WHITE", 'p');
