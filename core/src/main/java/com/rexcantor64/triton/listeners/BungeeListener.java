f9cdf006096d66506c15fb11c8018122337b0933
Diogo Correia <me@diogotc.com>
2021-01-27 16:14:17 +0000

    Add cancelled check on LoginEvent
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- core/src/main/java/com/rexcantor64/triton/listeners/BungeeListener.java
+++ core/src/main/java/com/rexcantor64/triton/listeners/BungeeListener.java
@@ -37,6 +37,7 @@ public class BungeeListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onLogin(LoginEvent event) {
+        if (event.isCancelled()) return;
         Plugin plugin = Triton.get().getLoader().asBungee();
         event.registerIntent(plugin);
         Triton.asBungee().getBungeeCord().getScheduler().runAsync(plugin, () -> {
