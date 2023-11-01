c7b49e7720ef78decbc62472a6e38cd390abf1f9
Diogo Correia <me@diogotc.com>
2023-11-01 17:45:06 +0100

    fix(core): null pointer exception when refreshing all players (#341)
    Fixes #326

--- core/src/main/java/com/rexcantor64/triton/Triton.java
+++ core/src/main/java/com/rexcantor64/triton/Triton.java
@@ -25,6 +25,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -109,8 +110,9 @@ public abstract class Triton implements com.rexcantor64.triton.api.Triton {
     }
 
     public void refreshPlayers() {
-        for (LanguagePlayer lp : playerManager.getAll())
-            lp.refreshAll();
+        playerManager.getAll().stream()
+                .filter(Objects::nonNull)
+                .forEach(LanguagePlayer::refreshAll);
     }
 
     public Configuration loadYAML(String fileName, String internalFileName) {
