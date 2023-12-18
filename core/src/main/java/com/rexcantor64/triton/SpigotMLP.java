dc27fcef293f523c14555c3a07317f074cc7b85f
Diogo Correia <me@diogotc.com>
2023-12-18 17:50:53 +0100

    fix(spigot): detect velocity modern forwarding for proxy warning (#367)
    Fixes #328
    
    Backport of aba7a9b to v3

--- core/src/main/java/com/rexcantor64/triton/SpigotMLP.java
+++ core/src/main/java/com/rexcantor64/triton/SpigotMLP.java
@@ -103,7 +103,7 @@ public class SpigotMLP extends Triton {
         }
 
         if (getConf().isBungeecord()) {
-            if (!isSpigotProxyMode()) {
+            if (!isSpigotProxyMode() && !isPaperProxyMode()) {
                 getLogger().logError("DANGER! DANGER! DANGER!");
                 getLogger().logError("Proxy mode is enabled on Triton but disabled on Spigot!");
                 getLogger().logError("A malicious player can run ANY command as the server.");
@@ -277,6 +277,29 @@ public class SpigotMLP extends Triton {
                 return false;
             }
             return (boolean) bungeeEnabled;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Use reflection to check if this Paper server has velocity modern forwarding enabled on paper-global.yml.
+     * This is used to show a warning if Paper is in proxy mode, but the server is not.
+     *
+     * @return Whether this Spigot server has velocity forwarding enabled on paper-global.yml.
+     */
+    public boolean isPaperProxyMode() {
+        try {
+            Class<?> paperConfigClass = Class.forName("io.papermc.paper.configuration.GlobalConfiguration");
+
+            Object instance = paperConfigClass.getMethod("get").invoke(null);
+            Object proxies = instance.getClass().getField("proxies").get(instance);
+            Object velocity = proxies.getClass().getField("velocity").get(proxies);
+            Object velocityEnabled = velocity.getClass().getField("enabled").get(velocity);
+            if (velocityEnabled == null) {
+                return false;
+            }
+            return (boolean) velocityEnabled;
         } catch (Exception e) {
             return false;
         }
