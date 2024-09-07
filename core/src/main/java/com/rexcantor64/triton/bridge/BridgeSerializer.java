b978981e3eec27344cbffc0100b7028c9c050525
Diogo Correia <me@diogotc.com>
2024-09-07 09:51:19 +0200

    fix: player kicked when plugin message is too big
    This happens if there is a translation bigger than the difference of the
    max size and threshold in Triton for the packet size.
    The threshold was lowered to (temporarily) fix this issue.
    
    A proper fix should be implemented in the future.
    
    Port of 9c7dd94290ec21911e02dba0a869aa6201d52a0e to v4

--- core/src/main/java/com/rexcantor64/triton/bridge/BridgeSerializer.java
+++ core/src/main/java/com/rexcantor64/triton/bridge/BridgeSerializer.java
@@ -107,10 +107,11 @@ public class BridgeSerializer {
             ByteArrayDataOutput languageItemsOut = ByteStreams.newDataOutput();
             for (val collection : Triton.get().getStorage().getCollections().values())
                 for (val item : collection.getItems()) {
-                    if (languageItemsOut.toByteArray().length > 29000) {
+                    // Max array size is 32767, leave out a buffer
+                    if (languageItemsOut.toByteArray().length > 25000) {
                         val out = ByteStreams.newDataOutput();
                         out.writeByte(ActionP2S.SEND_STORAGE_AND_CONFIG.getKey());
-                        if (outList.size() == 0) {
+                        if (outList.isEmpty()) {
                             out.writeBoolean(true);
                             out.write(languageOut);
                         } else {
@@ -187,7 +188,7 @@ public class BridgeSerializer {
                 }
             val out = ByteStreams.newDataOutput();
             out.writeByte(ActionP2S.SEND_STORAGE_AND_CONFIG.getKey());
-            val firstSend = outList.size() == 0;
+            val firstSend = outList.isEmpty();
             out.writeBoolean(firstSend);
             if (firstSend)
                 out.write(languageOut);
