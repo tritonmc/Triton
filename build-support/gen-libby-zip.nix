23640e57eae8cfb604547c6baf9072c1c3ed087a
Diogo Correia <me@diogotc.com>
2026-04-11 18:24:51 +0100

    chore: add scripts to generate zip archive with all libby dependencies
    This is a fail-safe in case of no internet connection or downtime
    of repo.diogotc.com.
    
    Closes #415

---
+++ build-support/gen-libby-zip.nix
@@ -0,0 +1,65 @@
+{
+  pkgs ? import <nixpkgs> { },
+  libsJson ? ../core/build/generated/libby-libs.json,
+  tritonVersion ? null,
+}:
+let
+  inherit (pkgs) lib;
+  libs = lib.importJSON libsJson;
+  repository = "https://repo.diogotc.com/mirror";
+  suffix = lib.optionalString (tritonVersion != null) "-${tritonVersion}";
+
+  mkJar =
+    {
+      groupId,
+      artifactId,
+      version,
+      sha256Checksum,
+    }:
+    let
+      groupId' = lib.replaceStrings [ "{}" ] [ "/" ] groupId;
+      jarPath = "${groupId'}/${artifactId}/${version}/${artifactId}-${version}.jar";
+    in
+    pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+      pname = artifactId;
+      inherit version;
+      src = pkgs.fetchurl {
+        url = "${repository}/${jarPath}";
+        hash = "sha256-${sha256Checksum}";
+      };
+
+      dontUnpack = true;
+
+      installPhase = ''
+        install -D -m 444 ${finalAttrs.src} $out/lib/${lib.escapeShellArg jarPath}
+      '';
+    });
+
+  allLibs = pkgs.symlinkJoin {
+    name = "triton-libs${suffix}";
+    paths = map mkJar libs;
+  };
+
+  readme = pkgs.writeText "triton-libs-readme${suffix}.txt" ''
+    This zip contains a copy of all libraries needed for Triton ${lib.defaultTo "<unknown>" tritonVersion}.
+    In case your server does not have an internet connection,
+    or the library distribution server is offline,
+    you may use this archive as a replacement.
+
+    To do so, copy the `lib` directory to the `plugins/Triton` directory (or `plugins/triton` on Velocity).
+  '';
+
+  libsZip = pkgs.runCommand "triton-libs-dist${suffix}" { } ''
+    dir="triton-libs${suffix}"
+    mkdir $dir
+    cp -rL ${allLibs}/lib $dir/lib
+    cp ${readme} $dir/README.txt
+    chmod -R u+rwX,go+rX $dir
+    mkdir -p $out
+    ${lib.getExe pkgs.zip} -r $out/"triton-libs${suffix}.zip" $dir
+  '';
+
+in
+{
+  inherit allLibs libsZip;
+}
