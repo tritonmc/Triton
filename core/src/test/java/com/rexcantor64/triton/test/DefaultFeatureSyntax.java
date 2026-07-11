0d4e3e3b1f1c83210cd3ac8ec3b5d2c48245d583
Diogo Correia <me@diogotc.com>
2026-07-12 00:30:59 +0100

    test: inherit core FeatureSyntax instead of api's for mock

---
+++ core/src/test/java/com/rexcantor64/triton/test/DefaultFeatureSyntax.java
@@ -0,0 +1,9 @@
+package com.rexcantor64.triton.test;
+
+import com.rexcantor64.triton.config.MainConfig;
+
+public class DefaultFeatureSyntax extends MainConfig.FeatureSyntax {
+    public DefaultFeatureSyntax() {
+        super("lang", "args", "arg");
+    }
+}
