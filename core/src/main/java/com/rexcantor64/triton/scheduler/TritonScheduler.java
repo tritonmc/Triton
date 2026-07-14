5e5c2405899eebf4e75e756245bc9184e9d7197f
Diogo Correia <me@diogotc.com>
2026-07-14 01:12:27 +0100

    docs: add javadocs to scheduler methods

--- core/src/main/java/com/rexcantor64/triton/scheduler/TritonScheduler.java
+++ core/src/main/java/com/rexcantor64/triton/scheduler/TritonScheduler.java
@@ -10,13 +10,46 @@ import org.jetbrains.annotations.NotNullByDefault;
 @NotNullByDefault
 public interface TritonScheduler {
 
+    /**
+     * Run a task in the server's sync/main thread.
+     *
+     * @param task The task to run in the sync thread.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     TaskHandler runSync(Runnable task);
 
+    /**
+     * Run a task in the server's sync/main thread after a given delay in server ticks.
+     * There are 20 ticks in a second.
+     *
+     * @param task The task to run in the sync thread.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     TaskHandler runSyncLater(Runnable task, long delayTicks);
 
+    /**
+     * Run a task in an async thread.
+     *
+     * @param task The task to run asynchronously.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     TaskHandler runAsync(Runnable task);
 
+    /**
+     * A reference to a scheduled task.
+     *
+     * @since 4.1.0
+     */
     interface TaskHandler {
+        /**
+         * Cancel the scheduled task.
+         * This method might be a no-op if the task cannot be canceled (e.g., it has already finished).
+         *
+         * @since 4.1.0
+         */
         void cancel();
     }
 }
--- triton-spigot/common/src/main/java/com/rexcantor64/triton/spigot/common/scheduler/BukkitGenericScheduler.java
+++ triton-spigot/common/src/main/java/com/rexcantor64/triton/spigot/common/scheduler/BukkitGenericScheduler.java
@@ -20,13 +20,48 @@ import java.util.concurrent.FutureTask;
 public abstract class BukkitGenericScheduler implements TritonScheduler {
     protected final Plugin plugin;
 
+    /**
+     * Run a task in the sync thread owned by the given entity.
+     * Depending on the platform, this might run on the main sync thread instead.
+     *
+     * @param entity The entity owning the thread this task should be run on.
+     * @param task The task to run in the sync thread.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     public abstract TaskHandler runSync(Entity entity, Runnable task);
 
+    /**
+     * Run a task in the sync thread owned by the given location.
+     * Depending on the platform, this might run on the main sync thread instead.
+     *
+     * @param location The location owning the thread this task should be run on.
+     * @param task The task to run in the sync thread.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     public abstract TaskHandler runSync(Location location, Runnable task);
 
+    /**
+     * Run a task in the sync thread owned by the given entity after a given delay in server ticks.
+     * There are 20 ticks in a second.
+     *
+     * @param entity The entity owning the thread this task should be run on.
+     * @param task The task to run in the sync thread.
+     * @return A reference to the scheduled task.
+     * @since 4.1.0
+     */
     public abstract TaskHandler runSyncLater(Entity entity, Runnable task, long delayTicks);
 
-
+    /**
+     * Run a computation on a sync/main thread and obtain its result.
+     *
+     * @param entity If applicable, use this entity to select the sync thread to run the computation on.
+     * @param task   The computation to run on the sync thread.
+     * @param <T>    The return type of the computation.
+     * @return The result of the computation, or an empty optional if it returned null or failed.
+     * @since 4.1.0
+     */
     public <T> Optional<T> callSync(Entity entity, Callable<@Nullable T> task) {
         try {
             if (this.isMainThreadOrOwnedBy(entity)) {
@@ -43,6 +78,15 @@ public abstract class BukkitGenericScheduler implements TritonScheduler {
         }
     }
 
+    /**
+     * Run a computation on a sync/main thread and obtain its result.
+     *
+     * @param location If applicable, use this location to select the sync thread to run the computation on.
+     * @param task     The computation to run on the sync thread.
+     * @param <T>      The return type of the computation.
+     * @return The result of the computation, or an empty optional if it returned null or failed.
+     * @since 4.1.0
+     */
     public <T> Optional<T> callSync(Location location, Callable<@Nullable T> task) {
         try {
             if (this.isMainThreadOrOwnedBy(location)) {
@@ -59,10 +103,33 @@ public abstract class BukkitGenericScheduler implements TritonScheduler {
         }
     }
 
+    /**
+     * Whether the current thread is synchronous.
+     * If both parameters are null, only checks if this is the main thread.
+     *
+     * @param entity   If given, also check whether the entity owns the current thread.
+     * @param location If given, also check whether the location owns the current thread.
+     * @return true if the current thread is the main thread or owned by one of the given parameters, false otherwise.
+     * @since 4.1.0
+     */
     public abstract boolean isSyncThread(@Nullable Entity entity, @Nullable Location location);
 
+    /**
+     * Whether the current thread is owned by the given entity.
+     *
+     * @param entity The entity to check thread ownership against.
+     * @return true if the given entity owns the current thread, false otherwise.
+     * @since 4.1.0
+     */
     public abstract boolean isMainThreadOrOwnedBy(Entity entity);
 
+    /**
+     * Whether the current thread is owned by the given location.
+     *
+     * @param location The location to check thread ownership against.
+     * @return true if the given location owns the current thread, false otherwise.
+     * @since 4.1.0
+     */
     public abstract boolean isMainThreadOrOwnedBy(Location location);
 
 }
